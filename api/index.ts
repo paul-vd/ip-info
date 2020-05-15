0baa9c0101369028e32ed55dbda67c8174fa8cb6
Paul van Dyk <paul@protocode.io>
2020-05-15 16:06:15 +0200

    update ip checkl

--- api/index.ts
+++ api/index.ts
@@ -1,30 +1,41 @@
 import { NowRequest, NowResponse } from '@now/node'
 import fetch from 'isomorphic-unfetch'
 
+function GetUserIP(req: NowRequest): string {
+  console.log(req.headers)
+  return ''
+}
+
 export default async (req: NowRequest, res: NowResponse) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
   try {
     if (req.method !== 'GET') throw new Error('Only GET methods are allowed')
     const queryIp = req.query.ip
 
-    let ip = queryIp || req.headers['x-forwarded-for'] || req.headers['CF-Connecting-IP']
+    let ip = queryIp as any
 
-    const xForwadedFor = req.headers['x-forwarded-for']
-    let testxForwadedFor
-    if (typeof xForwadedFor === 'string') {
-      testxForwadedFor = xForwadedFor.split(',')[0]
+    GetUserIP(req)
+
+    if (!ip) {
+      ip = req.headers['x-forwarded-for']
+      if (typeof ip === 'string') ip = ip.split(',')[0]
+      else ip = ip[0]
+    }
+
+    if (!ip || process.env.NODE_ENV === 'development') {
+      await fetch('https://api6.ipify.org').then(async (res: any) => (ip = await res.text()))
     }
 
     console.log({
+      realIP: ip,
       queryIp,
       cfConnectionIp: req.headers['CF-Connecting-IP'],
       xForwadedFor: req.headers['x-forwarded-for'],
-      testxForwadedFor,
     })
 
-    if (process.env.NODE_ENV === 'development') {
-      await fetch('https://api6.ipify.org').then(async (res) => (ip = await res.text()))
-    }
+    // if (process.env.NODE_ENV === 'development') {
+    //   await fetch('https://api6.ipify.org').then(async (res) => (ip = await res.text()))
+    // }
     if (!ip) throw new Error('No valid ip found')
     const response = await fetch(`https://freegeoip.app/json/${ip}`, {
       method: 'get',
